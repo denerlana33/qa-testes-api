58e2fb12406015cef1a297849844e0dc9c4ab712
dener <denerclaudino@hotmail.com>
2025-08-19 10:37:52 -0300

    Finalizado módulo 2 com testes de API: list, get e create

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,18 +1,34 @@
 import requests
 
+headers = {
+    "x-api-key": "reqres-free-v1"
+}
+
 def test_list_users(base_url):
-    response = requests.get(f"{base_url}/users?page=2")
+    response = requests.get(f"{base_url}/users?page=2", headers=headers)
+    print("LIST USERS STATUS:", response.status_code)
+    print("BODY:", response.text)
     assert response.status_code == 200
     assert "data" in response.json()
 
 def test_single_user(base_url):
-    response = requests.get(f"{base_url}/users/2")
+    response = requests.get(f"{base_url}/users/2", headers=headers)
+    print("SINGLE USER STATUS:", response.status_code)
+    print("BODY:", response.text)
     assert response.status_code == 200
     assert response.json()["data"]["id"] == 2
 
-#def test_user_not_found(base_url):
-    #response = requests.get(f"{base_url}/users/23")
-   # print(response.status_code)
- #   print(response.text)
-   # assert response.status_code == 404
-
+def test_create_user(base_url):
+    payload = {
+        "name": "morpheus",
+        "job": "leader"
+    }
+    response = requests.post(f"{base_url}/users", json=payload, headers=headers)
+    print("CREATE USER STATUS:", response.status_code)
+    print("BODY:", response.text)
+    assert response.status_code == 201
+    data = response.json()
+    assert data["name"] == "morpheus"
+    assert data["job"] == "leader"
+    assert "id" in data
+    assert "createdAt" in data
