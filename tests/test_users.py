c352e0d14b94cc543918e7f0d136e56748c35b5e
dener <denerclaudino@hotmail.com>
2025-08-21 08:59:09 -0300

    Corrigir testes para rodar com API pública no GitHub Actions

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,29 +1,26 @@
-import pytest
 import requests
 
 headers = {
     "x-api-key": "reqres-free-v1"
 }
 
-@pytest.fixture
-def base_url():
-    return "https://reqres.in/api"
+base_url = "https://reqres.in/api"
 
-def test_list_users(base_url):
+def test_list_users():
     response = requests.get(f"{base_url}/users?page=2", headers=headers)
     print("LIST USERS STATUS:", response.status_code)
     print("BODY:", response.text)
     assert response.status_code == 200
     assert "data" in response.json()
 
-def test_single_user(base_url):
+def test_single_user():
     response = requests.get(f"{base_url}/users/2", headers=headers)
     print("SINGLE USER STATUS:", response.status_code)
     print("BODY:", response.text)
     assert response.status_code == 200
     assert response.json()["data"]["id"] == 2
 
-def test_create_user(base_url):
+def test_create_user():
     payload = {
         "name": "morpheus",
         "job": "leader"
